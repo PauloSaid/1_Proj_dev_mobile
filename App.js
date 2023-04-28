edae4d8e97191a1e79d4d21a7dc5b17ef46e4c5f
Paulo Said <103071726+PauloSaid@users.noreply.github.com>
2023-04-27 21:26:52 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { Text, View } from "react-native";
+import styles from "./components/styles";
+import Filme from "./components/Filme/index";
+
+
+export default function App() {
+
+  let [filmes, setFilmes] = useState([]);
+
+  const baseURL = 'https://api.otaviolube.com/api/filmes?populate=*';
+
+
+
+  useEffect(function () {
+    fetch(baseURL).then(data => data.json()).then(objeto => {
+      setFilmes(objeto.data)
+    })
+  }, [])
+
+
+
+
+
+  return (
+    <View style={styles.main}>
+
+
+
+      <View style={styles.header}>
+        <Text style={styles.headerText}>FILMES UVV</Text>
+        <View style={styles.subHeader}>
+          <Text style={styles.subHeaderText}>Desenvolvimento Mobile</Text>
+        </View>
+      </View>
+
+      <View style={styles.filmes}>
+        {filmes.length > 0 ? filmes.map(filme => (
+          <Filme data={filme} />
+        )
+        ) : <Text>Carregando</Text>}
+      </View>
+
+
+    </View >
+  );
+}
