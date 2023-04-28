32e3887ec2178ad5e1d33ef489b0534164dd7cd3
Paulo Said <103071726+PauloSaid@users.noreply.github.com>
2023-04-28 10:55:39 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import styles from "./components/styles";
 import Filme from "./components/Filme/index";
+import SearchBox from "./components/Filme/SearchBox";
 
 
 export default function App() {
@@ -25,14 +26,14 @@ export default function App() {
   return (
     <View style={styles.main}>
 
-
-
       <View style={styles.header}>
         <Text style={styles.headerText}>FILMES UVV</Text>
         <View style={styles.subHeader}>
           <Text style={styles.subHeaderText}>Desenvolvimento Mobile</Text>
         </View>
       </View>
+
+      <SearchBox/>
 
       <View style={styles.filmes}>
         {filmes.length > 0 ? filmes.map(filme => (
