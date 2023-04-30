efdf663d1d5985a8c20fcb2e1bb0a7420ac27726
Paulo Said <103071726+PauloSaid@users.noreply.github.com>
2023-04-30 02:13:02 -0300

    Add files via upload

--- components/styles.js
+++ components/styles.js
@@ -1,6 +1,7 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
-
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
     main: {
@@ -16,51 +17,66 @@ const styles = StyleSheet.create({
     headerText: {
         color: '#A49E8D',
         backgroundColor: '#353535',
-        width: '100%',
-        fontSize: '300%',
+        width: windowWidth,
+        fontSize: windowWidth * 0.05,
     },
     subHeader: {
         backgroundColor: '#8F8778',
         flexDirection: 'row',
         justifyContent: 'center',
-        borderBottomEndRadius: 15,
-        borderBottomStartRadius: 15,
+        borderBottomEndRadius: windowHeight * 0.03,
+        borderBottomStartRadius: windowHeight * 0.03,
     },
     subHeaderText: {
-        fontSize: '125%',
+        fontSize: windowWidth * 0.03,
         fontWeight: '500',
     },
     filmes: {
         backgroundColor: "#3c6e71",
         justifyContent: "center",
         alignItems: "center",
-        height: '100%',
+        height: windowHeight,
         flex: 1,
     },
     searchContainer: {
-        paddingHorizontal: 10,
-        paddingVertical: 5,
+        alignItems: 'center',
+        paddingHorizontal: windowWidth * 0.0050,
+        paddingVertical: windowHeight * 0.0050,
         backgroundColor: '#fff',
-        borderRadius: 5,
-        marginHorizontal: 20,
-        marginTop: 15,
+        borderRadius: windowHeight * 0.01,
+        marginHorizontal: windowWidth * 0.1,
+        marginTop: windowHeight * 0.015,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.5,
         shadowRadius: 3.84,
         elevation: 5,
-        marginBottom: 2,
         borderBottomColor: 'black',
-        borderBottomWidth: 2,
-      },
-      searchBox: {
-        fontSize: 16,
+        borderBottomWidth: windowHeight * 0.003,
+    },
+    searchBox: {
+        fontSize: windowWidth * 0.035,
         fontWeight: '400',
-        paddingHorizontal: 10,
-        paddingVertical: 5,
+        paddingHorizontal: windowWidth * 0.04,
+        paddingVertical: windowHeight * 0.02,
         color: '#000',
-      },
-});
+    },
+    status: {
+        flex: 1,
+        textAlign: 'center',
+        marginTop: 15,
 
+    },
+    checar: {
+        flex: 1,
+        alignItems: 'center',
+        width: '50%',
+        marginTop: 10,
+    },
+    headerBox: {
+        textAlign: 'center',
+        fontWeight: 500,
+    },
+});
 
 export default styles;
