10b0f49229f5030c9555aa069a55ee932d9f484d
Paulo Said <103071726+PauloSaid@users.noreply.github.com>
2023-05-19 10:47:56 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,48 +1,18 @@
-import { useEffect, useState } from "react";
-import { Text, View } from "react-native";
-import styles from "./components/styles";
-import Filme from "./components/Filme/index";
-import SearchBox from "./components/Filme/SearchBox";
+import Main from "./src/components/pages/MainPage/index";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import LoginPage from "./src/components/pages/LoginPage";
+
+const Stack = createStackNavigator();
 
 
 export default function App() {
-
-  let [filmes, setFilmes] = useState([]);
-
-  const baseURL = 'https://api.otaviolube.com/api/filmes?populate=*';
-
-
-
-  useEffect(function () {
-    fetch(baseURL).then(data => data.json()).then(objeto => {
-      setFilmes(objeto.data)
-    })
-  }, [])
-
-
-
-
-
-  return (
-    <View style={styles.main}>
-
-      <View style={styles.header}>
-        <Text style={styles.headerText}>FILMES UVV</Text>
-        <View style={styles.subHeader}>
-          <Text style={styles.subHeaderText}>Desenvolvimento Mobile</Text>
-        </View>
-      </View>
-
-      <SearchBox />
-
-      <View style={styles.filmes}>
-        {filmes.length > 0 ? filmes.map(filme => (
-          <Filme data={filme} />
-        )
-        ) : <Text>Carregando</Text>}
-      </View>
-
-
-    </View >
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name = "MainPage" component={Main}/>
+        <Stack.Screen name = "LoginPage" component={LoginPage}/>  
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
